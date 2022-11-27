93d8f522eadfacf08703726e6661da9b181a2543
Nicolas Lopez <nicolas@comnic.com.ar>
2022-11-27 19:53:05 -0300

    entrega final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Category } from "./pages/Category";
+import { Detail } from "./pages/Detail";
+import { UserLayout } from "./components/UserLayout";
+import { CartProvider } from "./context/cartContext";
+import { Cart } from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<UserLayout />}>
+              <Route index element={<Home />} />
+              <Route path={"/category/:categoryId"} element={<Category />} />
+              <Route path={"/product/:productId"} element={<Detail />} />
+              <Route path="/cart" element={<Cart />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </div>
   );
 }
